f6e4dd5432fc519c503b44a795556c5cd1afb9d6
jain <jainroyp@gmail.com>
2025-11-25 20:16:21 +0530

    gh pages

--- index.tsx
+++ index.tsx
@@ -1,15 +1,9 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css"; // if you have Tailwind CSS file
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
